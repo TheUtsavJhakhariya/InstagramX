fc89a460eca770d4c0ebff591c32291a39b95d2b
Utsav Jhakhariya <86795590+TheUtsavJhakhariya@users.noreply.github.com>
2023-11-08 00:38:47 -0500

    Create insta-story.js

---
+++ insta-story.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+        const screens = document.querySelectorAll('.story-screen');
+
+        function showScreen(index) {
+            screens.forEach(screen => screen.style.display = 'none');
+            screens[index].style.display = 'flex';
+        }
+
+        function next() {
+            currentIndex = (currentIndex + 1) % screens.length;
+            showScreen(currentIndex);
+        }
+
+        function prev() {
+            currentIndex = (currentIndex - 1 + screens.length) % screens.length;
+            showScreen(currentIndex);
+        }
+
+        setInterval(() => {
+            next();
+        }, 2000);
+
+        function goBack() {
+        window.location.href = 'untitled Github (114).html';
+        }
